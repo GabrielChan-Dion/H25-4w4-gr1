d7567d797028bd628801250fd6c1a15adbf3c897
GabrielChan-Dion <120771005+GabrielChan-Dion@users.noreply.github.com>
2025-03-18 13:26:12 -0400

    Ajout d'une variable de couleur, remplacement des anciennes couleurs

--- category.php
+++ category.php
@@ -5,10 +5,9 @@
  */
 ?>
 <?php get_header() ?>
-<h1>Category.php</h1>
     <section class="populaire">
         <div class="boiteflex global">
-        <h1><?php single_cat_title(); ?></h1>
+        <h1 class="populaire__titre__category"><?php single_cat_title(); ?></h1>
         <p><?php echo category_description() ?></p>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <?php get_template_part("gabarit/carte"); ?>
