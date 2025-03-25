c3ba248a7553b887866a0ada8c2b1589505ddfef
GabrielChan-Dion <120771005+GabrielChan-Dion@users.noreply.github.com>
2025-03-25 11:09:54 -0400

    Ajout des icones réseaux sociaux et du scss pour les icones

--- 404.php
+++ 404.php
@@ -11,9 +11,16 @@
         <div class="page404__div">
             <H1 class="page404__titre">Erreur 404</H1>
             <p class="page404__description"> <?php echo $Page404_description?></p>
-            <h2 class="page404__titre_secondaire">Vous ne trouvez pas ce que vous chercher?</h2>
-            <p>Essayer à nouveau ou explorer nos autres options!</p>
+            <h2 class="page404__titre__secondaire">Vous ne trouvez pas ce que vous chercher?</h2>
+            <p class="page404__titre__sousTitre">Essayer à nouveau ou explorer nos autres options!</p>
             <div class="page404__recherche"><?php get_search_form();?></div>
+            <h2 class="page404__titre__secondaire">Vous ne trouvez quand même pas ce que vous chercher?<br>Rejoingnez-nous sur nos Réseaux sociaux!</h2>
+            <div class="page404__iconeapp">
+                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
+            </div>
           
         </div>
     </section>
