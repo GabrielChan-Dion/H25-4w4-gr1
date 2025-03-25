e1904e524c0916c295b10e9788ecef817f0c4e0e
GabrielChan-Dion <120771005+GabrielChan-Dion@users.noreply.github.com>
2025-03-25 11:53:52 -0400

    Modification et ajout de la section 404-menu et modification scss

--- 404.php
+++ 404.php
@@ -13,11 +13,19 @@
             <div class="page404__background" style="background-image: url('<?php echo $Page404_background ?>'); background-repeat: no-repeat">
                 <H1 class="page404__titre">Erreur 404</H1>
             </div>
-        
-            <p class="page404__description"> <?php echo $Page404_description?></p>
-            <h2 class="page404__titre__secondaire">Vous ne trouvez pas ce que vous chercher?</h2>
-            <p class="page404__titre__sousTitre">Essayer à nouveau ou explorer nos autres options!</p>
-            <div class="page404__recherche"><?php get_search_form();?></div>
+            <div class="page404__menu">
+                <p class="page404__description"> <?php echo $Page404_description?></p>
+                    <h2 class="page404__titre__secondaire">Vous ne trouvez pas ce que vous chercher?</h2>
+                    <p class="page404__titre__sousTitre">Essayer à nouveau ou explorer nos autres options!</p>
+                    <?php wp_nav_menu(array(
+                                    'menu' => 'principal',
+                                    'container' => 'nav',
+                                    'container_class' => 'entete__menu',
+                        )); ?>
+                <div class="page404__recherche"><?php get_search_form();?></div>
+            </div>
+            
+            
             <h2 class="page404__titre__secondaire">Vous ne trouvez quand même pas ce que vous chercher?<br>Rejoingnez-nous sur nos Réseaux sociaux!</h2>
             <div class="page404__iconeapp">
                 <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
