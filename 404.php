3107ee3fb4cba0af2efa84ca77f199d98c94ce2b
GabrielChan-Dion <120771005+GabrielChan-Dion@users.noreply.github.com>
2025-04-01 10:50:01 -0400

    Modification scss 404

--- 404.php
+++ 404.php
@@ -10,15 +10,21 @@
     $Page404_background = get_theme_mod("Page404_background", 'Default Title');
 ?>
 <?php get_header() ?>
-    <section class="page404">
-        <div class="page404__div" style="background-image: url('<?php echo $Page404_background ?>'); background-repeat: no-repeat">
+    <section class="page404" style="background-image: url('<?php echo $Page404_background ?>'); background-repeat: no-repeat; background-size: cover;" >
+        
                 <H1 class="page404__titre"><?php echo $Page404_title?></H1>
                 <p class="page404__description"> <?php echo $Page404_description?></p>
                     
+                
+                <nav class="page404__nav">
+                    <?php wp_nav_menu(array(
+                                'menu' => 'Page404',
+                                'container' => 'nav',
+                                'container_class' => 'page404__menu',
+                    )); ?>
+                </nav>
                 <div class="page404__recherche"><?php get_search_form();?></div>
-            
           
-        </div>
     </section>
     <?php get_footer(); ?>
 </body>
