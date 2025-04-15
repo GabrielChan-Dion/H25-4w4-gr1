ea2a30caf1f9bf75ccee41ff7bc9b59c31ea40f3
GabrielChan-Dion <120771005+GabrielChan-Dion@users.noreply.github.com>
2025-04-15 11:05:21 -0400

    Ajout du Carrousel et le changement avec les inputs

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,9 +1,27 @@
-(function(){
-console.log ("carrousel.js")
-let hero__radio__input = document.querySelectorAll(".hero__radio__input")
-console.log("hero__radio__input.length = ", hero__radio__input.length)
+(function () {
+    let hero__radio__input = document.querySelectorAll(".hero__radio__input");
+    console.log("hero__radio__input.length = ", hero__radio__input.length)
+    let hero__carrousels = document.querySelectorAll(".hero__carrousel");
 
-hero__radio__input.forEach(elm=>{
-    console.log("elm.id_carrousel = ",elm.dataset.id_carrousel)
-})
-})()
+    hero__radio__input.forEach(elm=>{
+        console.log("elm.id_carrousel = ",elm.dataset.id_carrousel)
+    })
+
+    hero__radio__input.forEach(elm => {
+        elm.addEventListener('change', () => {
+            if (elm.checked) {
+                const index = elm.dataset.id_carrousel;
+
+                hero__carrousels.forEach(carrousel => {
+                    carrousel.classList.remove("hero__carrousel--active");
+                });
+
+                const target = hero__carrousels[index];
+                if (target) {
+                    target.classList.add("hero__carrousel--active");
+                }
+            }
+        });
+    });
+    document.querySelector(".hero__radio__input:checked")?.dispatchEvent(new Event("change"));
+})();
