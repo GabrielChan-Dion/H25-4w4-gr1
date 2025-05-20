74dc5553fb1dc0528afc4410aa1acb3f18d33431
GabrielChan-Dion <120771005+GabrielChan-Dion@users.noreply.github.com>
2025-05-20 13:52:46 -0400

    Ajout des températures, date et auteur dans single.php

--- single.php
+++ single.php
@@ -3,6 +3,7 @@
  *  index.php est le modèle par défaut
  *  si aucun modèle peut satisfaire la requête http dans ce cas c'est index.php qui affichera le contenu de la page
  */
+$hero_auteur = get_theme_mod('hero_auteur', 'Default Title'); 
 ?>
 <?php get_header() ?>
     <section class="populaire__single">
@@ -14,6 +15,11 @@
                 the_post_thumbnail(); ?>
                 <h2 class="populaire__titre__single"><?php the_title(); ?></h2>
                 <div class="populaire__contenu__single"><?php the_content(); ?></div>
+                <p>Auteur: <?php echo $hero_auteur;  ?></p>
+                <p>Date de publication: <?php  echo the_field ('date_publication'); ?></p>
+                <p>Température minimum&nbsp;<?php  echo the_field ('temperature_minimum'); ?>&#xB0;C</p>
+                <p>Température maximum&nbsp;<?php echo the_field ('temperature_maximum'); ?>&#xB0;C</p>
+                <p>Température moyenne&nbsp;<?php echo the_field ('temperature_moyenne'); ?>&#xB0;C</p>
             </article>
             <?php endwhile; endif; ?>
         </div>
